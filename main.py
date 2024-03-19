5d9fbacc26c23cc37b6000ce39265289dbe6ade1
s-ddhant <149889288+s-ddhant@users.noreply.github.com>
2024-03-19 15:05:09 +0530

    testing

--- main.py
+++ main.py
@@ -1,17 +1,20 @@
 import pygame
+import os
 
 pygame.init()
-
 
 SCREEN_WIDTH = 800
 SCREEN_HEIGHT = int(SCREEN_WIDTH * 0.8)
 
 screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
-pygame.display.set_caption('Python game')
+pygame.display.set_caption('Shooter')
 
 #set framerate
 clock = pygame.time.Clock()
 FPS = 60
+
+#define game variables
+GRAVITY = 0.75
 
 #define player action variables
 moving_left = False
@@ -19,22 +22,38 @@ moving_right = False
 
 #define colours
 BG = (144, 201, 120)
+RED = (255, 0, 0)
 
 def draw_bg():
 	screen.fill(BG)
+	pygame.draw.line(screen, RED, (0, 300), (SCREEN_WIDTH, 300))
 
 class Soldier(pygame.sprite.Sprite):
 	def __init__(self, char_type, x, y, scale, speed):
-		pygame.sprite.Sprite.__init__(self)
 		self.char_type = char_type
 		self.speed = speed
 		self.direction = 1
 		self.flip = False
-		img = pygame.image.load(f'img/{self.char_type}/Idle/0.png')
-		self.image = pygame.transform.scale(img, (int(img.get_width() * scale), int(img.get_height() * scale)))
+		self.animation_list = []
+		self.frame_index = 0
+		self.action = 0
+		self.update_time = pygame.time.get_ticks()
+		
+		#load all images for the players
+		animation_types = ['Idle', 'Run', 'Jump']
+		for animation in animation_types:
+			#reset temporary list of images
+			temp_list = []
+			#count number of files in the folder
+			num_of_frames = len(os.listdir(f'img/{self.char_type}/{animation}'))
+			for i in range(num_of_frames):
+				img = pygame.image.load(f'img/{self.char_type}/{animation}/{i}.png')
+				img = pygame.transform.scale(img, (int(img.get_width() * scale), int(img.get_height() * scale)))
+				temp_list.append(img)
+			self.animation_list.append(temp_list)
+		self.image = self.animation_list[self.action][self.frame_index]
 		self.rect = self.image.get_rect()
 		self.rect.center = (x, y)
-
 
 	def move(self, moving_left, moving_right):
 		#reset movement variables
@@ -51,10 +70,22 @@ class Soldier(pygame.sprite.Sprite):
 			self.flip = False
 			self.direction = 1
 
-
 		#update rectangle position
 		self.rect.x += dx
 		self.rect.y += dy
+
+	def update_animation(self):
+		#update animation
+		ANIMATION_COOLDOWN = 100
+		#update image depending on current frame
+		self.image = self.animation_list[self.action][self.frame_index]
+		#check if enough time has passed since the last update
+		if pygame.time.get_ticks() - self.update_time > ANIMATION_COOLDOWN:
+			self.update_time = pygame.time.get_ticks()
+			self.frame_index += 1
+		#if the animation has run out the reset back to the start
+		if self.frame_index >= len(self.animation_list[self.action]):
+			self.frame_index = 0
 
 	def draw(self):
 		screen.blit(pygame.transform.flip(self.image, self.flip, False), self.rect)
@@ -67,13 +98,11 @@ while run:
 
 	clock.tick(FPS)
 
-	#USE THIS TO RESET THE BACKGROUND SO THE OLD IMAGE GETS OVERLAPPED
 	draw_bg()
 
+	player.update_animation()
 	player.draw()
 	enemy.draw()
-
-	player.move(moving_left, moving_right)
 
 	for event in pygame.event.get():
 		#quit game
@@ -85,9 +114,10 @@ while run:
 				moving_left = True
 			if event.key == pygame.K_d:
 				moving_right = True
+			if event.key == pygame.K_w and player.alive:
+				player.jump = True
 			if event.key == pygame.K_ESCAPE:
 				run = False
-
 
 		#keyboard button released
 		if event.type == pygame.KEYUP:
@@ -97,4 +127,5 @@ while run:
 				moving_right = False
 
 	pygame.display.update()
+
 pygame.quit()

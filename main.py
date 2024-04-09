7a70321d0dc33ec65b3cd0389105922f2fae202e
s-ddhant <149889288+s-ddhant@users.noreply.github.com>
2024-04-09 22:06:53 +0530

    lil updates

--- main.py
+++ main.py
@@ -3,22 +3,24 @@ import os
 
 pygame.init()
 
+
 SCREEN_WIDTH = 800
 SCREEN_HEIGHT = int(SCREEN_WIDTH * 0.8)
 
 screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
-pygame.display.set_caption('2-D platformer')
+pygame.display.set_caption('Shooter')
 
-#set framerate to cap how fast things would play
+#set framerate
 clock = pygame.time.Clock()
 FPS = 60
 
-#define game physics
+#define game variables
 GRAVITY = 0.75
 
 #define player action variables
 moving_left = False
 moving_right = False
+
 
 #define colours
 BG = (144, 201, 120)
@@ -27,6 +29,8 @@ RED = (255, 0, 0)
 def draw_bg():
 	screen.fill(BG)
 	pygame.draw.line(screen, RED, (0, 300), (SCREEN_WIDTH, 300))
+
+
 
 class Soldier(pygame.sprite.Sprite):
 	def __init__(self, char_type, x, y, scale, speed):
@@ -49,10 +53,10 @@ class Soldier(pygame.sprite.Sprite):
 		for animation in animation_types:
 			#reset temporary list of images
 			temp_list = []
-			#count number of files in the folder which would determine the number of frames in the animation
+			#count number of files in the folder
 			num_of_frames = len(os.listdir(f'img/{self.char_type}/{animation}'))
 			for i in range(num_of_frames):
-				img = pygame.image.load(f'img/{self.char_type}/{animation}/{i}.png')
+				img = pygame.image.load(f'img/{self.char_type}/{animation}/{i}.png').convert_alpha()
 				img = pygame.transform.scale(img, (int(img.get_width() * scale), int(img.get_height() * scale)))
 				temp_list.append(img)
 			self.animation_list.append(temp_list)
@@ -60,6 +64,7 @@ class Soldier(pygame.sprite.Sprite):
 		self.image = self.animation_list[self.action][self.frame_index]
 		self.rect = self.image.get_rect()
 		self.rect.center = (x, y)
+
 
 	def move(self, moving_left, moving_right):
 		#reset movement variables
@@ -97,6 +102,7 @@ class Soldier(pygame.sprite.Sprite):
 		self.rect.x += dx
 		self.rect.y += dy
 
+
 	def update_animation(self):
 		#update animation
 		ANIMATION_COOLDOWN = 100
@@ -110,6 +116,8 @@ class Soldier(pygame.sprite.Sprite):
 		if self.frame_index >= len(self.animation_list[self.action]):
 			self.frame_index = 0
 
+
+
 	def update_action(self, new_action):
 		#check if the new action is different to the previous one
 		if new_action != self.action:
@@ -118,19 +126,30 @@ class Soldier(pygame.sprite.Sprite):
 			self.frame_index = 0
 			self.update_time = pygame.time.get_ticks()
 
+
+
 	def draw(self):
 		screen.blit(pygame.transform.flip(self.image, self.flip, False), self.rect)
+
+
 
 player = Soldier('player', 200, 200, 3, 5)
 enemy = Soldier('enemy', 400, 200, 3, 5)
 
+
+
 run = True
 while run:
+
 	clock.tick(FPS)
+
 	draw_bg()
+
 	player.update_animation()
 	player.draw()
- 
+	enemy.draw()
+
+
 	#update player actions
 	if player.alive:
 		if player.in_air:
@@ -140,6 +159,7 @@ while run:
 		else:
 			player.update_action(0)#0: idle
 		player.move(moving_left, moving_right)
+
 
 	for event in pygame.event.get():
 		#quit game
@@ -156,12 +176,16 @@ while run:
 			if event.key == pygame.K_ESCAPE:
 				run = False
 
+
 		#keyboard button released
 		if event.type == pygame.KEYUP:
 			if event.key == pygame.K_a:
 				moving_left = False
 			if event.key == pygame.K_d:
 				moving_right = False
+
+
+
 
 	pygame.display.update()
 
---
+++ tempCodeRunnerFile.py
@@ -0,0 +1,4 @@
+
+		#apply gravity
+		self.vel_y += GRAVITY
+		if self.vel_y > 10:
